26bfcfcee01cd7756ea8c65d0b01c1a46af814d5
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2022-07-23 12:49:43 -0300

    quarto commit - implementação do analisador sintático

--- analisador-sintatico/parser.py
+++ analisador-sintatico/parser.py
@@ -1,5 +1,53 @@
 from sly import Parser
+from lexeranalyser import LexerAnalyser
 
-class Parser(Parser):
-    # Get the token list from the lexer (required)
-    tokens = CalcLexer.tokens
+
+class ParserAnalyser(Parser):
+    # Carregar a lista de tokens do analisador lexico
+    tokens = LexerAnalyser.tokens
+
+    # Grammar rules and actions
+    @_('expr PLUS term')
+    def expr(self, p):
+        return p.expr + p.term
+
+    @_('expr MINUS term')
+    def expr(self, p):
+        return p.expr - p.term
+
+    @_('term')
+    def expr(self, p):
+        return p.term
+
+    @_('term TIMES factor')
+    def term(self, p):
+        return p.term * p.factor
+
+    @_('term DIVIDE factor')
+    def term(self, p):
+        return p.term / p.factor
+
+    @_('factor')
+    def term(self, p):
+        return p.factor
+
+    @_('NUMBER')
+    def factor(self, p):
+        return p.NUMBER
+
+    @_('LPAREN expr RPAREN')
+    def factor(self, p):
+        return p.expr
+
+
+if __name__ == '__main__':
+    lexer = LexerAnalyser()
+    parser = ParserAnalyser()
+
+    while True:
+        try:
+            text = input('calc > ')
+            result = parser.parse(lexer.tokenize(text))
+            print(result)
+        except EOFError:
+            break
