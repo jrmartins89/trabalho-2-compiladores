359280f99a3edac6fdf82fe933fb9688b71a880f
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2022-07-26 01:54:22 -0300

    23 commit - Ajuste no main pra criar o laço do parser

--- analisador-sintatico/main.py
+++ analisador-sintatico/main.py
@@ -1,8 +1,19 @@
 from lexeranalyser import GCCLexer
+from gccparser import GCCParser
 
 if __name__ == '__main__':
     data = 'x = 3 + 42 * (s - t)'
     lexer = GCCLexer()
     for tok in lexer.tokenize(data):
         print('type=%r, value=%r' % (tok.type, tok.value))
+
+    parser = GCCParser()
+
+    while True:
+        try:
+            text = input('calc > ')
+            result = parser.parse(lexer.tokenize(text))
+            print(result)
+        except EOFError:
+            break
 
