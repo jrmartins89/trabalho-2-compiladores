cafb110ebb4c0c5f20e2676799aaf9f5bfe77634
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2022-07-26 15:50:21 -0300

    24 commit - alteração do input do analisador lexico

--- analisador-sintatico/main.py
+++ analisador-sintatico/main.py
@@ -2,7 +2,22 @@ from gcclexer import GCCLexer
 from gccparser import GCCParser
 
 if __name__ == '__main__':
-    data = 'x = 3 + 42 * (s - t)'
+    data = 'def func1 (int A, int B){' \
+           'int SM [2];' \
+           'SM [0] = A + B;' \
+           'SM [1] = B * C;' \
+           'return ;' \
+           '}' \
+           'def principal ()' \
+           '{' \
+           'int C;' \
+           'int D;' \
+           'int R;' \
+           'C = 4;' \
+           'D = 5;' \
+           'R = func1 (C, D);' \
+           'return ;' \
+           '}'
     lexer = GCCLexer()
     for tok in lexer.tokenize(data):
         print('type=%r, value=%r' % (tok.type, tok.value))
