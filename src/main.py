51d2c407efe9369269af11bd6b3bc1bb650f482b
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2022-07-30 00:00:19 -0300

    4 commit - Ajuste do loop do main

--- src/main.py
+++ src/main.py
@@ -5,15 +5,19 @@ from gccparser import GCCParser
 
 if __name__ == '__main__':
     lexer = GCCLexer()
+    text = True
     while True:
         try:
             if len(sys.argv) <= 1:
-                print('################Analisador Lexico########################\n')
+                print('\n\n################Analisador Lexico########################\n')
                 text = input('Insira o caminho do arquivo: '
                              ' ../../trabalho-2-compiladores/codigosLCC/codigo1.lcc \n'
                              'Para acessar os outros arquivos basta trocar o nome do arquivo para os nomes '
-                             '"codigo2.lcc" ou "codigo3.lcc" no final do caminho do arquivo.\n\n'
+                             '"codigo2.lcc" ou "codigo3.lcc" no final do caminho do arquivo. Digite "sair" para sair do programa\n\n'
                              'caminho ->')
+                if text == 'sair':
+                    text = False
+                    break
             lexText = open(text, 'r').read()
             print(lexText)
         except EOFError:
@@ -23,15 +27,14 @@ if __name__ == '__main__':
                 lexResult = lexer.tokenize(lexText)
                 for token in lexResult:
                     print(token)
-                break
         except lex.LexError as e:
             print(e)
 
     print('\n\n\n\n')
 
-    print('################Analisador Sintático########################\n\n')
-    parser = GCCParser()
-    while True:
+    while text:
+        print('################Analisador Sintático########################\n\n')
+        parser = GCCParser()
         try:
             result = parser.parse(lexer.tokenize(lexText))
             print(result)
